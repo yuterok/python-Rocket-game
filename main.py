9d7462683f99213a3125b459233d79d09d49af8d
Julia Tereschenko <yuterok@gmail.com>
2022-05-23 01:11:39 +0300

    Collide, new score count, High score logic, Rocket sprite
    Добавила проверку коллизии ракеты с астероидами + астероид исчезает при соприкосновении, переделала подсчет очков, теперь он не связан с главным счётчиком (иначе score не останавливался после проигрыша), добавила high score с использованием чтения и записи из txt, сделала спрайт ракеты

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import bgcolor
 import pygame
 import random
 import time
@@ -8,9 +9,14 @@ pygame.init()
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
 FPS = 30
-f = open('high_score.txt','r')
-print(*f)
-high_score = 0
+
+game_over = False
+lifes = 3
+time_score = 0
+
+h = open('high_score.txt','r')
+high_score = h.read()
+h.close()
 
 window = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('название пока не придумала')
@@ -19,12 +25,25 @@ bg = pygame.image.load('img/bg.png')
 font = pygame.font.Font('Baloo.ttf', 45)
 h_font = pygame.font.Font('Baloo.ttf', 25)
 
-rocket = pygame.image.load('img/pink.png')
 rocket_x = SCREEN_WIDTH/2
 rocket_y = 500
 rocket_width = 100
 rocket_height = 191
 speed = 20
+color = 'img/yellow.png'
+
+class Rocket(pygame.sprite.Sprite):
+    def __init__(self, x, y, filename):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(filename)
+        self.rect = self.image.get_rect(center=(x,y))
+
+# без этого координаты rect ракеты не будут обновляться
+    def update(self):
+        self.rect.x = rocket_x
+        self.rect.y = rocket_y
+
+rocket = Rocket(rocket_x, rocket_y, color)
 
 asteroids_speed = 10
 asteroids_x = [100, 450, 200, 900, 200]
@@ -37,6 +56,7 @@ class Asteroid(pygame.sprite.Sprite):
         self.image = pygame.image.load(filename)
         self.rect = self.image.get_rect(center=(x,y))
 
+# бесконечная генерация астероидов при выходе за пределы экрана
     def update(self, asteroids_speed):
         self.rect.y += asteroids_speed  
         if self.rect.y > SCREEN_HEIGHT+10:
@@ -53,12 +73,12 @@ asteroids_list.add(asteroid1,asteroid2,asteroid3,asteroid4,asteroid5)
 def drawWindow():
 
     window.blit(bg, (0,0))
-    window.blit(rocket, (rocket_x,rocket_y))
+    window.blit(rocket.image, (rocket_x,rocket_y))
     asteroids_list.draw(window)
     window.blit(score_text, (20,10))
     window.blit(high_score_text, (20,70))
-
-    pygame.display.update()
+    window.blit(lifes_text, (850,30))
+    
 
 run = True
 while run:
@@ -68,35 +88,63 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    keys = pygame.key.get_pressed()
-    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and rocket_x > 5:
-        rocket_x -= speed
-    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and rocket_x < SCREEN_WIDTH-rocket_width:
-        rocket_x += speed
-    if (keys[pygame.K_UP] or keys[pygame.K_w]) and rocket_y>100:
+    if not game_over:
+        keys = pygame.key.get_pressed()
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and rocket_x > 5:
+            rocket_x -= speed
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and rocket_x < SCREEN_WIDTH-rocket_width:
+            rocket_x += speed
+        if (keys[pygame.K_UP] or keys[pygame.K_w]) and rocket.rect.y>100:
             rocket_y -= speed
-    if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and rocket_y < SCREEN_HEIGHT-rocket_height:
+        if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and rocket_y < SCREEN_HEIGHT-rocket_height:
             rocket_y += speed
-    if (keys[pygame.K_ESCAPE]):
-        clock.tick(0.01)
-          
-    asteroids_list.update(asteroids_speed)
-    asteroids_speed += 0.02
-    
-    score = round((pygame.time.get_ticks())/100 + asteroids_speed*10)
-    score_text = font.render(f'Score: {score}', True, (251,246,121))
-    high_score_text = h_font.render(f'High score: {high_score}', True, (251,246,121))
-    
-    if score == 200:
-        asteroids_list.add(Asteroid(random.randint(0,SCREEN_WIDTH), -50, 'img/asteroid1.png'))
-    if score == 500:
-        asteroids_list.add(Asteroid(random.randint(0,SCREEN_WIDTH), -50, 'img/asteroid2.png'))
-    if score == 750:
-        asteroids_list.add(Asteroid(random.randint(0,SCREEN_WIDTH), -50, 'img/asteroid3.png'))
+        if (keys[pygame.K_ESCAPE]):
+                pygame.quit()
 
-    # print(round((pygame.time.get_ticks())/50 + asteroids_speed))
-    # print(asteroids_speed)
-    # print(asteroids_list)
-    drawWindow()
+        rocket.update()
+        asteroids_list.update(asteroids_speed)
+        asteroids_speed += 0.02
+
+        time_score +=1
+        
+        score = round(time_score + asteroids_speed*50)-500
+
+        score_text = font.render(f'Score: {score}', True, (251,246,121))
+        high_score_text = h_font.render(f'High score: {high_score}', True, (251,246,121))
+        lifes_text = font.render(f'Lifes: {lifes}', True, (255, 107, 107))
+
+    # усложнение игры по мере прохождения
+        if score == 200:
+            asteroids_list.add(Asteroid(random.randint(0,SCREEN_WIDTH), -50, 'img/asteroid1.png'))
+        if score == 500:
+            asteroids_list.add(Asteroid(random.randint(0,SCREEN_WIDTH), -50, 'img/asteroid2.png'))
+        if score == 750:
+            asteroids_list.add(Asteroid(random.randint(0,SCREEN_WIDTH), -50, 'img/asteroid3.png'))
+
+    # проверка столкновений
+        asteroid_hit_list = pygame.sprite.spritecollide(rocket, asteroids_list, False)
+        for asteroid in asteroid_hit_list:
+            asteroid.rect.y = SCREEN_HEIGHT+100
+            lifes -=1
+        
+        if lifes == 0:
+            game_over = True
+
+        drawWindow()
+
+    if game_over:
+        window.blit(bg, (0,0))
+        window.blit(score_text, (400,300))
+        asteroids_speed = 0
+        speed = 0
+        time_score = 0
+        win_text = font.render('Ты установил новый рекорд!!', True, (251,246,121))
+        if high_score =="" or score > int(high_score):
+            h = open('high_score.txt','w')
+            h.write(str(score))
+            h.close()
+            window.blit(win_text, (200,200))
+
+    pygame.display.update()
 
 pygame.quit()
